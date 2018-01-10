f0e699bf6f4832fdcbc0e7200dc5e0456713cd3e
Liza Floyd <liza199797@mail.ru>
2018-01-10 15:53:30 +0300

    small blog done

--- js/app.js
+++ js/app.js
@@ -1,62 +1,63 @@
 var my_news = [
-	{
-		author: 'Саша Печкин',
-		text: 'В четверг, четвертого числа...'
-	},
-	{
-		author: 'Просто Вася',
-		text: 'Считаю, что $ должен стоить 35 рублей!'
-	},
-	{
-		author: 'Гость',
-		text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
-	}
+{
+author: 'Саша Печкин',
+text: 'В четчерг, четвертого числа...'
+},
+{
+author: 'Просто Вася',
+text: 'Считаю, что $ должен стоить 35 рублей!'
+},
+{
+author: 'Гость',
+text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+}
 ];
-
+var Article = React.createClass({
+	render: function() {
+var author = this.props.data.author,
+text = this.props.data.text;
+return (
+<div className='article'>
+<p className='news__author'>{author}:</p>
+<p className='news__text'>{text}</p>
+</div>
+)
+}
+});
 var News = React.createClass({
-	render: function() {
-		var data = this.props.data;
-		var newsTemplate = data.map(function(item, index) {
-			return (
-				<div key={index}>
-					<p className="news__author">{item.author}:</p>
-					<p className="news__text">{item.text}</p>
-				</div>
-			)
-		})
-
-		return (
-			<div className="news">
-				{newsTemplate}
-			</div>
-		);
-	}
+render: function() {
+var data = this.props.data;
+var newsTemplate;
+if (data.length > 0) {
+newsTemplate = data.map(function(item, index) {
+return (
+<div key={index}>
+<Article data={item} />
+</div>
+)
+})
+} else {
+newsTemplate = <p>К сожалению новостей нет</p>
+}
+return (
+<div className='news'>
+{newsTemplate}
+<strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+</div>
+);
+}
 });
-
-
-var Comments = React.createClass({
-	render: function() {
-		return (
-			<div className="comments">
-				Нет новостей - комментировать нечего
-			</div>
-		);
-	}
-});
-
 var App = React.createClass({
-	render: function() {
-		return (
-			<div className="app">
-				Всем привет, я компонент App! Я умею отображать новости.
-			<News data={my_news} />
-			<Comments />
-			</div>
-		);
-	}
+render: function() {
+return (
+<div className='app'>
+<h3>Новости</h3>
+<News data={my_news} />
+</div>
+);
+}
 });
-
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+<App />,
+document.getElementById('root')
 );
